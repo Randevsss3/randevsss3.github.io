a942607bafeec2ddce72a73276f03c12e6cf2d94
Randevsss3 <resturamadhan9156@gmail.com>
2021-09-17 14:15:39 +0700

    Ini yg file dart

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_design/homePge.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
